a868df0587c5f8cdcc50dcff4499a74c405e5549
robertozanchi <r.zanchi@gmail.com>
2015-12-22 17:21:51 +0100

    conplete bio, work and project sections

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,165 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+// bio JSON 
+var bio = {
+	"name" : "Roberto Zanchi",
+	"role" : "Web Developer",
+	"contacts" : {
+		"mobile" : "1234556",
+		"email" : "r.zanchi@gmail.com",
+		"github" : "robertozanchi",
+		"twitter" : "@robertozanchi",
+		"location" : "Berlin, Germany"
+	},
+	"welcomeMessage" : "Full stack web developer in training at Udacity. Passionate about solving societal and environmental problems, with eight years’ business consulting and non-profit experience in Asia and Europe.",
+	"skills" : [
+	"HTML", "CSS", "JavaScript", "Swift"
+	],
+	"pic" : "images/rz.jpg"
+}
+
+// bio function
+bio.display = function() {
+	var formattedName = HTMLheaderName.replace("%data%", bio.name);
+	var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+	var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+	var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+	var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts["twitter"]);
+	var formattedLocation = HTMLlocation.replace("%data%", bio.contacts["location"]);
+	var formattedBioPic = HTMLbioPic.replace("%data%", bio["pic"]);
+	var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+	$("#header").prepend(formattedRole);
+	$("#header").prepend(formattedName);
+	$("#topContacts").append(formattedLocation);
+    $("#topContacts").append(formattedEmail);
+    $("#topContacts").append(formattedGithub);
+	$("#topContacts").append(formattedTwitter);	
+	$("#header").append(formattedBioPic);
+	$("#header").append(formattedWelcomeMsg);
+
+    if (bio.skills.length > 0) {
+    	$("#header").append(HTMLskillsStart);
+        for (skill in bio.skills) {
+        	var formattedSkill = HTMLskills.replace("%data%", bio.skills[skill]);
+            $("#skills").append(formattedSkill);
+        }
+    }
+
+}
+
+// work JSON
+var work = {
+  "jobs": [
+    {
+  	"employer" : "CDP",
+  	"title" : "Technical Manager",
+  	"dates" : "March 2014 - present",
+  	"location" : "Berlin, Germany",
+  	"description" : "Manage a technical working group of 8 experts to guide 50 multinational corporations on renewable energy options."
+    },
+    {
+    "employer" : "The Indo-Italian Chamber of Commerce and Industry",
+    "title" : "Project Manager",
+    "dates" : "August 2011 - April 2012",
+    "location" : "Mumbai, India",
+    "description" : "Created opportunities for 20+ Italian SMEs, helping them to increase sales and launch joint ventures in India."
+    }
+  ]
+}
+
+// work function
+work.display = function() {
+	for (job in work.jobs) {
+		$("#workExperience").append(HTMLworkStart);
+		var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+		var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+		var formattedEmployerTitle = formattedEmployer + formattedTitle;
+		$(".work-entry:last").append(formattedEmployerTitle);
+
+		var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+	    $(".work-entry:last").append(formattedDates);
+
+	    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+	    $(".work-entry:last").append(formattedDescription);
+
+	}
+}
+
+// projects JSON
+var projects = {
+  "projects" : [
+    {
+    "title" : "Resume",
+    "dates" : "2015",
+    "description" : "A resume in HTML, CSS and JavaScript"
+    }
+  ]
+}
+
+// projects function
+projects.display = function() {
+	for (project in projects.projects) {
+		$("#projects").append(HTMLprojectStart);
+
+		var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+		$(".project-entry:last").append(formattedTitle);
+
+		var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+		$(".project-entry:last").append(formattedDates);
+
+		var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+		$(".project-entry:last").append(formattedDescription);
+
+		if (projects.projects[project].images.length > 0) {
+			for (image in projects.projects[project.images]) {
+				var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+				$(".project-entry:last").append(formattedImage);
+			}
+		}
+	}
+}
+
+// education JSON
+var education = {
+	"schools": [
+		{
+			"name": "Imperial College London",
+			"location": "London, UK",
+			"degree": "Energy Policy, MSc",
+			"dates": "2012-2013"		
+		}
+	],
+	"onlineCourses": [
+		{
+			"title": "Full Stack Web Developer Nanodegree",
+			"school": "Udacity",
+			"dates": "2015"
+		}
+	]
+};
+
+// education function
+education.display = function() {
+	for (school in education.schools) {
+		$("#education").append(HTMLschoolStart);
+
+        var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[school].name);
+        var formattedDegree = HTMLschoolDegree.replace("%data%", education.schools[school].degree);
+        var formattedDates = HTMLschoolDates.replace("%data%", education.schools[school].dates);
+        var formattedLocation = HTMLschoolLocation.replace("%data%", education.schools[school].location);
+		$(".education-entry:last").append(formattedDates);
+		$(".education-entry:last").append(formattedSchoolName);
+		$(".education-entry:last").append(formattedDegree);
+		$(".education-entry:last").append(formattedLocation);
+
+	}
+
+}
+
+// call bio, work, education, projects functions
+bio.display();
+work.display();
+projects.display();
+education.display();
+
+$("#main").append(internationalizeButton);
+$("#mapDiv").append(googleMap);
